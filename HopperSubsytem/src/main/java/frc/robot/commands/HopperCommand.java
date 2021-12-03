4829bd1948dc0ccc9c56559f7378bfae9c8eef9a
Calvin Tucker <me@calvintucker.com>
2021-12-02 18:50:30 -0600

    e

--- HopperSubsytem/src/main/java/frc/robot/commands/HopperCommand.java
+++ HopperSubsytem/src/main/java/frc/robot/commands/HopperCommand.java
@@ -2,19 +2,20 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.subsystems;
+package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.HopperSubsystem;
 
-public class ShooterCommand extends CommandBase {
-  private ShooterSubsystem m_shooterSubsystem;
+public class HopperCommand extends CommandBase {
+  private HopperSubsystem m_hopperSubsystem;
   private boolean toggle = true;
   /** Creates a new CommandScheduler. */
-  public ShooterCommand(ShooterSubsystem h) {
+  public HopperCommand(HopperSubsystem h) {
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(h);
-    m_shooterSubsystem = h;
+    m_hopperSubsystem = h;
   }
 
   // Called when the command is initially scheduled.
@@ -32,8 +33,8 @@ public class ShooterCommand extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    m_shooterSubsystem.teleopShooterPeriodic(toggle);
-    m_shooterSubsystem.teleopHoodPeriodic(toggle);
+    m_hopperSubsystem.teleopIntakePeriodic(toggle);
+    
   }
 
   // Called once the command ends or is interrupted.
--- HopperSubsytem/src/main/java/frc/robot/commands/ShooterCommand.java
+++ HopperSubsytem/src/main/java/frc/robot/commands/ShooterCommand.java
@@ -2,19 +2,20 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.subsystems;
+package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.ShooterSubsystem;
 
-public class HopperCommand extends CommandBase {
-  private HopperSubsystem m_hopperSubsystem;
+public class ShooterCommand extends CommandBase {
+  private ShooterSubsystem m_shooterSubsystem;
   private boolean toggle = true;
   /** Creates a new CommandScheduler. */
-  public HopperCommand(HopperSubsystem h) {
+  public ShooterCommand(ShooterSubsystem h) {
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(h);
-    m_hopperSubsystem = h;
+    m_shooterSubsystem = h;
   }
 
   // Called when the command is initially scheduled.
@@ -32,8 +33,8 @@ public class HopperCommand extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    m_hopperSubsystem.teleopIntakePeriodic(toggle);
-    
+    m_shooterSubsystem.teleopShooterPeriodic(toggle);
+    m_shooterSubsystem.teleopHoodPeriodic(toggle);
   }
 
   // Called once the command ends or is interrupted.
